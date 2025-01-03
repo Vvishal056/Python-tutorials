a3b84e362909e5e33e23b91389e6efa63dbffa68
Bishal Bhandari <visbalb56@gmail.com>
2025-01-03 09:44:38 +0545

    Add files via upload
    you can see multiple files and folder here what i have done is used modules and package so i can use a function or class in different file by importing it where to use

---
+++ mymodule.py
@@ -0,0 +1,13 @@
+def useful_func():
+  print("using the useful_func")
+
+class useful_class():
+   
+  def __init__(self,message):
+    self.message = message 
+
+  def report(self):
+    print(self.message)
+
+
+  
---
+++ myprogram.py
@@ -0,0 +1,3 @@
+from mypackage.submodule import my_sub_func
+
+my_sub_func()
