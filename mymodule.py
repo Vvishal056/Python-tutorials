a8aed02a4084be60e2191fcd55925483cae9f5e3
Bishal Bhandari <visbalb56@gmail.com>
2025-01-04 08:48:50 +0545

    Add files via upload
    these files are linked with each other as in here i have learned how to install package and about link modules

--- mymodule.py
+++ mymodule.py
@@ -1,13 +1,2 @@
-def useful_func():
-  print("using the useful_func")
 
-class useful_class():
    
-  def __init__(self,message):
-    self.message = message 
-
-  def report(self):
-    print(self.message)
-
-
-  
