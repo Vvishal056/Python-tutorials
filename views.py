eeba532cb293e02b216298fe7d14edf2a59af75d
Bishal Bhandari <visbalb56@gmail.com>
2025-01-09 15:47:02 +0545

    Add files via upload
    in this file i have learned about http404 not response by installing class package of http404

--- views.py
+++ views.py
@@ -1,6 +1,21 @@
-from django.http.response import HttpResponse
+from django.shortcuts import render
+from django.http.response import HttpResponse,HttpResponseNotFound, Http404
+# Create your views here.
+articles = {
+   'sports':'sports page',
+   'finance':'finance page',
+   'polities':'politics page'
+}
 
-def home_view(request):
-  return HttpResponse("home view")
+def news_view(request,topic):
+   try:
+      result = articles[topic]
+      return HttpResponse(articles[topic])
+   except:
+      raise Http404("404 error")
 
-  
+
+def add_view(request,num1,num2):
+   add_result = num1 + num2
+   result =  "{num1} + {num2}= {add_result}"
+   return HttpResponse(str(result))
